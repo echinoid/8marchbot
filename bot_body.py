4f76a698ff5e660de2620167e8ca26d2a79b5c0c
Aleksei Ivanov <87769670+echinoid@users.noreply.github.com>
2023-03-04 16:45:54 +0100

    Add handler for start

--- bot_body.py
+++ bot_body.py
@@ -5,10 +5,9 @@ API_TOKEN = '6250638796:AAEC7HNHcvuj-K1Bej0nyuPto97TrM5Hrmk'
 bot = telebot.TeleBot(API_TOKEN)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['start'])
 def start(message):
-    if message.text:
-        bot.send_message(message.from_user.id, 'aaaaaaaaa')
+    bot.send_message(message.from_user.id, 'aaaaaaaaa')
 
 
 bot.polling(none_stop=True, interval=0)
